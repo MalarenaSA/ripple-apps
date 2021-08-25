0c02b201e2c69b4f256f17ee762b1739692468ef
MKen212 <46496040+MKen212@users.noreply.github.com>
2021-08-25 19:21:56 +0200

    Browser Transaction Display

--- browser/js/monitor.js
+++ browser/js/monitor.js
@@ -31,12 +31,13 @@ const LOG_TRANS = true;  // Flag to output transaction data to console
 let currentServer = "Testnet";  // Current Server -- TODO: GET FROM LOCAL STORAGE
 let currentServerURL = SERVERS[currentServer];  // Current Server URL to connect to
 let reserveBaseXRP = 0;  // Minimum XRP Reserve per account
-let awaitingMsgs = {};  // Object to hold Awaiting Messages
-let receivedTrans = {};  // Object to hold Received Transactions
+let awaitingMsgs = {};  // Awaiting Messages
+let receivedTrans = {};  // Received Transactions
+let currentTrans = "";  // Current Selected Transaction
 let messageID = 0;  // Specific message ID
 let currentAccounts = [{  // Current Accounts -- TODO: GET FROM LOCAL STORAGE
   "name": "New",
-  "address": "rfyJRyFZzX71LL5LreHpUZBZqrB18xUL4P",
+  "address": "rsCXXu88g3LtHgVmVj1obnY6gzvUdRyxZN",
   "transClass": "justify-content-start",
 }, {
   "name": "Testnet02",
@@ -48,9 +49,11 @@ let currentAccounts = [{  // Current Accounts -- TODO: GET FROM LOCAL STORAGE
   "transClass": "justify-content-end",
 }];
 
+// rsCXXu88g3LtHgVmVj1obnY6gzvUdRyxZN  - NEW
 // rPT1Sjq2YGrBMTttX4GZHjKu9dyfzbpAYe  - Faucet
-// rPRHmwvH6zFpgGYZKXWWrQ6DRojBQ14MFA - New
-// rww9WLeWwviNvAJV3QeRCof3kJLomPnaNw - Testnet01
+// rfyJRyFZzX71LL5LreHpUZBZqrB18xUL4P  - Offers
+// rww9WLeWwviNvAJV3QeRCof3kJLomPnaNw  - Testnet01
+// rhacBEhAdTBeuwcXe5ArVnX8Kwh886poSo  - Testnet02
 // rnbsExCdXV2y85Qg9ewKkuNsuQGGjDBfBC  - Testnet03
 
 // Global HTML Elements
@@ -60,7 +63,6 @@ const ledgerInfoEl = document.getElementById("ledgerInfo");
 const accountInfoEls = document.querySelectorAll(".account-info");
 const accountTransEls = document.querySelectorAll(".account-trans");
 const transInfoEl = document.getElementById("transInfo");
-
 
 // Initialise Bootstrap Tooltips
 const tooltipList = document.querySelectorAll(`[data-bs-toggle="tooltip"]`);
@@ -75,10 +77,8 @@ const socket = new WebSocket(currentServerURL);
 // Add Websocket "close" event listener
 socket.addEventListener("close", () => {
   console.log(`Disconnected from '${currentServer}' at '${currentServerURL}'`);
-  // Show disconnected message, but only if no error message
-  if (!statusEl.lastChild.classList.contains("col-error")) {
-    showStatus("text-warning", "Disconnected");
-  }
+  // Show disconnected message
+  showStatus("text-warning", "Disconnected");
 });
 
 // Add Websocket "error" event listener
@@ -121,8 +121,13 @@ socket.addEventListener("open", () => {
 const refreshBtn = document.getElementById("refreshBtn");
 refreshBtn.addEventListener("click", refreshInfo);
 
-// Function to refresh info
-function refreshInfo(){
+// Function to Refresh Info
+function refreshInfo(event = null){
+  if (event) {
+    // Clear any error message if refresh was triggered by click event
+    statusEl.lastChild.classList.remove("col-error");
+  }
+  // Get all information data
   getServerInfo();
   getLedgerInfo();
   getAccountsInfo();
@@ -234,12 +239,15 @@ async function getLedgerInfo() {
   } else {
     ledgerAgeClass = "col-error";
   }
+
+  // Fix Ledger Close Time
+  const ledgerCloseTime = fixDate(new Date((RIPPLE_EPOCH + response.result.ledger.close_time) * 1000));
   
   // Update Ledger Info Table
   ledgerInfoEl.innerHTML = `
     <tr><th>Ledger Version:</th><td>${response.result.ledger.ledger_index} ${ledgerValid}</td></tr>
     <tr><th>Age:</th><td class="${ledgerAgeClass}">${ledgerAge.toFixed(4)}s</td></tr>
-    <tr><th>Close Time:</th><td>${fixDate(new Date((RIPPLE_EPOCH + response.result.ledger.close_time) * 1000))}</td></tr>
+    <tr><th>Close Time:</th><td>${ledgerCloseTime}</td></tr>
     <tr><th>Transaction Count:</th><td>${response.result.ledger.transactions.length}</td></tr>
   `;
 }
@@ -278,7 +286,7 @@ function getAccountsInfo() {
       </thead>
       <tbody>
         <tr><th>Address:</th><td>${account.address}</td></tr>
-        <tr><th>Balance:</th><td><span class=${accBalClass}>${accBalXRP.toFixed(6).toString()}</span> XRP as at Ledger '${response.result.ledger_index}'</td></tr>
+        <tr><th>Balance:</th><td><span class=${accBalClass}>${accBalXRP.toFixed(6)}</span> XRP as at Ledger '${response.result.ledger_index}'</td></tr>
       </tbody>
     `;
   });
@@ -286,9 +294,13 @@ function getAccountsInfo() {
 
 // Function to Subscribe to Accounts Transactions
 async function subscribeActTrans() {
+  // Create Accounts Array
+  const accountsArray = currentAccounts.map((account) => {
+    return account.address;
+  });
   const message = {
     "command": "subscribe",
-    "accounts": currentAccounts.map((account) => { return account.address; }),
+    "accounts": accountsArray,
   };
   const response = await createRequest(message, "sub");
   if (LOG_INFO === true) {
@@ -306,64 +318,86 @@ async function subscribeActTrans() {
 
 // Function to process a Websocket Transaction
 function processTransaction(data) {
-  // Store Transaction Data for future use & output to console
-  receivedTrans[data.transaction.hash] = data;
-  if (LOG_TRANS === true) {
-    console.log(data);
-  }
-
-  // Fix Transaction Date
-  const transDate = fixDate(new Date((RIPPLE_EPOCH + data.transaction.date) * 1000));
-
-  // Fix Transaction Delivered Amount
-  const transAmtXRP = convertDropsToXRP(data.meta.delivered_amount);
-
-  // Loop over accounts and output transaction details for relevant account
-  currentAccounts.forEach((account, index) => {
-    let transType = "";
-    if (account.address === data.transaction.Account) {
-      transType = "Sent";
-    } else if (account.address === data.transaction.Destination) {
-      transType = "Received";
+  try {
+    // Store Transaction Data for future use & output to console
+    receivedTrans[data.transaction.hash] = data;
+    if (LOG_TRANS === true) {
+      console.log(data);
     }
-
-    if (transType !== "") {
-      const newTransItem = document.createElement("p");
-      const newTransLink = document.createElement("a");
-      newTransLink.href = "#";
-      newTransLink.className = "px-2";
-      // Add Data Attributes for Transaction Hash and Account Index
-      const hashAttr = document.createAttribute("data-hash");
-      hashAttr.value = data.transaction.hash;
-      newTransLink.setAttributeNode(hashAttr);
-      const accountAttr = document.createAttribute("data-account");
-      accountAttr.value = index;
-      newTransLink.setAttributeNode(accountAttr);
-
-      if (data.meta.TransactionResult !== "tesSUCCESS") {
-        // Failed Transaction
-        newTransLink.classList.add("trans-error");
-        newTransLink.textContent = `${transDate} > ${transType} Transaction Failed! See Console`;
-
-      } else if (data.transaction.TransactionType !== "Payment" || typeof data.meta.delivered_amount !== "string") {
-        // Non-XRP Payment Sent
-        newTransLink.classList.add("trans-non-payment");
-        newTransLink.textContent = `${transDate} > Non-XRP Payment ${transType} - See Console`;
-
-      } else {
-        // Show Sent Payment Transaction
-        newTransLink.classList.add("trans-sent");
-        newTransLink.textContent = `${transDate} > ${transAmtXRP.toFixed(6)} XRP ${transType}`;
+  
+    // Fix Transaction Date
+    const transDate = fixDate(new Date((RIPPLE_EPOCH + data.transaction.date) * 1000));
+  
+    // Fix Transaction Delivered Amount
+    const transAmtXRP = convertDropsToXRP(data.meta.delivered_amount);
+  
+    // Loop over accounts and output transaction details for relevant account
+    let accountFound = false;
+    currentAccounts.forEach((account, index) => {
+      let transText = "";
+      if (account.address === data.transaction.Account) {
+        transText = "Sent";
+        accountFound = true;
+      } else if (account.address === data.transaction.Destination) {
+        transText = "Received";
+        accountFound = true;
       }
+  
+      if (transText !== "") {
+        // Create new <p> and <a> elements and populate with default values
+        const newTransItem = document.createElement("p");
+        const newTransLink = document.createElement("a");
+        newTransLink.href = "#";
+        newTransLink.className = "px-2";
+        // Add Data Attributes for Transaction Hash, Account Index & Trans Type
+        const dataHash = document.createAttribute("data-hash");
+        dataHash.value = data.transaction.hash;
+        newTransLink.setAttributeNode(dataHash);
+        const dataAccount = document.createAttribute("data-account");
+        dataAccount.value = index;
+        newTransLink.setAttributeNode(dataAccount);
+        const dataType = document.createAttribute("data-type");
+        dataType.value = transText;
+        newTransLink.setAttributeNode(dataType);
+  
+        if (data.meta.TransactionResult !== "tesSUCCESS") {
+          // Failed Transaction
+          newTransLink.classList.add("trans-error");
+          newTransLink.textContent = `${transDate} > ${transText} Transaction Failed!`;
+  
+        } else if (data.transaction.TransactionType !== "Payment" || typeof data.meta.delivered_amount !== "string") {
+          // Non-XRP Payment Sent
+          newTransLink.classList.add("trans-non-payment");
+          newTransLink.textContent = `${transDate} > Non-XRP Payment ${transText}`;
+  
+        } else {
+          // Show Sent Payment Transaction
+          newTransLink.classList.add(`trans-${transText.toLowerCase()}`);
+          newTransLink.textContent = `${transDate} > ${transAmtXRP.toFixed(6)} XRP ${transText}`;
+        }
+  
+        // Update Transaction List with new item
+        newTransItem.appendChild(newTransLink);
+        accountTransEls[index].prepend(newTransItem);
+  
+        // Add "click" Event Listener to new item
+        accountTransEls[index].addEventListener("click", showTransaction);
+      }
+    });
 
-      // Update Transaction List with new item
-      newTransItem.appendChild(newTransLink);
-      accountTransEls[index].prepend(newTransItem);
-
-      // Add "click" Event Listener to new item
-      accountTransEls[index].addEventListener("click", showTransaction);
+    // Check that transaction was for one of the subscribed accounts
+    if (accountFound === false) {
+      throw new Error (`Transaction received for non-subscribed account`);
     }
-  });
+
+  } catch (error) {
+    console.error(`Error processing transaction: ${error}`);
+    // Only log transaction data to console if not already being logged to console
+    if (LOG_TRANS === false) {
+      console.log(data);
+    }
+    showStatus("col-error", "Error. See console");
+  }
 
   // Refresh Information Data
   refreshInfo();
@@ -371,44 +405,89 @@ function processTransaction(data) {
 
 // Function to show a selected transaction
 function showTransaction(event) {
+  // Check if currentTrans already selected and if so close it
+  if (currentTrans !== "") {
+    closeTransaction();
+
+    // STILL PROBLEM HERE - CANNOT READ PROPERTY IF PROPERLY CLOSED
+    
+  }
+  currentTrans = event.target;
   const transHash = event.target.dataset.hash;
   const transData = receivedTrans[transHash];
-  const transAmount = convertDropsToXRP(transData.meta.delivered_amount);
   const accountIndex = event.target.dataset.account;
+  const transType = event.target.dataset.type;
 
+  // Fix Transaction Date
+  const transDate = fixDate(new Date((RIPPLE_EPOCH + transData.transaction.date) * 1000));
+
+  // Calculate Transaction Amount
+  const transAmount = (Object.prototype.hasOwnProperty.call(transData.meta, "delivered_amount")) ? convertDropsToXRP(transData.meta.delivered_amount) : 0;
+
+  // Calculate Fee Amount if Type is "Sent"
+  let feeText = "";
+  if (transType === "Sent") {
+    const transFee = convertDropsToXRP(transData.transaction.Fee);
+    feeText = ` (+Fee: ${transFee.toFixed(6)} XRP)`;
+  }
+
+  // Get Result Class
+  const resultClass = (transData.meta.TransactionResult === "tesSUCCESS") ? "col-success" : "col-error";
+
+  // Update Transaction Table
   transInfoEl.innerHTML = `
     <div class="row ${currentAccounts[accountIndex].transClass}">
       <div class="col-6 border">
         <table class="table table-sm table-dark">
           <thead>
             <tr><th class="text-center" colspan="2" scope="colgroup">
-              <h5 class="text-info">Transaction: ${currentAccounts[accountIndex].name} @ ${fixDate(new Date((RIPPLE_EPOCH + transData.transaction.date) * 1000))}
-                <button type="button" class="float-end btn-close bg-light h6 my-1" id="transClose"></button>
+              <h5 class="text-info">Transaction: ${currentAccounts[accountIndex].name} @ ${transDate}
+                <button type="button" class="float-end btn-close bg-light h6 my-1" id="transCloseBtn"></button>
               </h5>
             </th></tr>
           </thead>
           <tbody>
+            <tr><th>Type:</th><td>${transData.transaction.TransactionType} (${transType})</td></tr>
             <tr><th>Hash:</th><td>${transHash}</td></tr>
-            <tr><th>From/To:</th><td>${transData.transaction.Account} TO ${transData.transaction.Detination}</td></tr>
-            <tr><th>Amount:</th><td>95.12345 XRP (+Fee: 0.000012 XRP)</td></tr>
-            <tr><th>Result:</th><td class="col-success">tesSUCCESS</td></tr>
+            <tr><th>From/To:</th><td>${transData.transaction.Account} TO ${transData.transaction.Destination}</td></tr>
+            <tr><th>Amount:</th><td>${transAmount.toFixed(6)} XRP${feeText}</td></tr>
+            <tr><th>Result:</th><td class="${resultClass}">${transData.meta.TransactionResult} - ${transData.engine_result_message}</td></tr>
           </tbody>
         </table>
       </div>
     </div>
   `;
 
-  
-  // TO HERE - LOOK AT MAKING FIXDATE WORK BETTER 
-  
+  // Set class of selected transaction
+  currentTrans.classList.remove(`trans-${transType.toLowerCase()}`);
+  currentTrans.classList.add(`trans-${transType.toLowerCase()}-selected`);
 
+  // Add Close Button Event Listener
+  const transCloseBtn = document.getElementById("transCloseBtn");
+  transCloseBtn.addEventListener("click", closeTransaction);
+
+  // Function for Transaction Close Button
+  function closeTransaction() {
+    // Hide Transaction Info Section
+    transInfoEl.style.visibility = "hidden";
+    // Re-Set class of selected transaction & clear currentTrans
+    currentTrans.classList.remove(`trans-${currentTrans.dataset.type.toLowerCase()}-selected`);
+    currentTrans.classList.add(`trans-${currentTrans.dataset.type.toLowerCase()}`);
+    currentTrans = "";
+  }
+
+  // Show Transaction Info Section
+  transInfoEl.style.visibility = "visible";
 }
 
 
 // Function to show the current status message
 function showStatus(statusClass, message) {
-  const statusDate = fixDate(new Date());
-  statusEl.innerHTML = `<span class="col-info">Last updated: ${statusDate} - </span><span class="${statusClass}">${message}`;
+  // Only show new message if current message is not an error
+  if (!statusEl.lastChild.classList.contains("col-error")) {
+    const statusDate = fixDate(new Date());
+    statusEl.innerHTML = `<span class="col-info">Last updated: ${statusDate} - </span><span class="${statusClass}">${message}`;  
+  }
 }
 
 // Function to fix the output date format
