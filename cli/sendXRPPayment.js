5f4ec292bf9483fde6ecb781825ffc0f2bee0b4b
MKen212 <46496040+MKen212@users.noreply.github.com>
2021-09-09 16:10:47 +0200

    CLI Added sendXRPPayment app

---
+++ cli/sendXRPPayment.js
@@ -0,0 +1,122 @@
+"use strict";
+
+// sendXRPPayments.app - Create, Sign & Send an XRP Payment Transaction
+
+// Load Env Variables
+const Dotenv = require("dotenv");
+const dotenvConfig = Dotenv.config();
+if (dotenvConfig.error) console.error(dotenvConfig.error);
+
+// Load Node Readline functionality
+const Readline = require("readline");
+const rl = Readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Load ripple-lib API
+const RippleAPI = require("ripple-lib").RippleAPI;
+
+// Configure API
+const api = new RippleAPI({
+  server: process.env.XRPL_SERVER
+});
+
+// Handle API Connection Errors
+api.on("error", (errorCode, errorMessage, data) => {
+  console.error(`API Connection Error:`);
+  console.error(`${errorCode} : ${errorMessage} : ${data}`);
+});
+
+// Handle Connection
+api.on("connected", () => {
+  console.log(`Connected to server: ${process.env.XRPL_SERVER}\n`);
+});
+
+// Handle Disconnection
+api.on("disconnected", (code) => {
+  console.log(`Disconnected from server with code: ${code}\n`);
+  // Close readline
+  rl.close();
+});
+
+
+// Connect to Server and process request
+let preparedTx = {};  // Prepared Transaction
+let signedTx = {};  // Signed Transaction
+
+api.connect().then(() => {
+  // Prepare Transaction
+  const transaction = {
+    "TransactionType": "Payment",
+    "Account": process.env.XRPL_ADDRESS,
+    "Amount": process.env.XRPL_AMT_DROPS,
+    "Destination": process.env.XRPL_DESTINATION
+  };
+  const instructions = {
+    "maxLedgerVersionOffset": 50  // ~ 3-4mins
+  };
+  return api.prepareTransaction(transaction, instructions);
+}).then((response) => {
+  // Display preparedTx & ask to continue
+  preparedTx = response;
+  showMessage("PreparedTx", (JSON.parse(preparedTx.txJSON)));
+  showMessage("Instructions", (preparedTx.instructions));
+  return ask("Sign above transaction (Yes/No)? ");
+}).then(() => {
+  // Sign transaction
+  return api.sign(preparedTx.txJSON, process.env.XRPL_SECRET);
+}, () => {
+  // Do not sign transaction
+  throw "Signing Process Terminated.";
+}).then((response) => {
+  // Display signedTx & ask to continue
+  signedTx = response;
+  showMessage("SignedTx", signedTx);
+  return ask("Submit above signed transaction (Yes/No)? ");
+}).then(() => {
+  // Submit transaction
+  return api.submit(signedTx.signedTransaction);
+}, () => {
+  // Do not submit transaction
+  throw "Sending Process Terminated.";
+}).then((response) => {
+  // Display tentative result
+  showMessage("Tentative Result", response);
+  console.log(`See monitor, XRPL Explorer or CLI:getTransaction() for final status.`);
+  console.log(`Link: ${process.env.XRPL_EXPLORER}transactions/${response.tx_json.hash}`);
+  console.log(`CLI: node getTransactionInfo.js ${response.tx_json.hash}\n`);
+}).then(() => {
+  // Disconnect from the server
+  return api.disconnect();
+}).catch((error) => {
+  // Handle response errors
+  if (typeof error === "object") {
+    console.error("Response returned an Error:");
+  }
+  console.error(error);
+  return api.disconnect();
+});
+
+
+// Function to display formatted messages on the console
+function showMessage(title, message) {
+  console.log(`---------- ${title} ----------`);
+  console.log(message);
+  console.log(`========== \\${title} ==========\n`);
+}
+
+// Function to ask the user a question
+function ask(question) {
+  return new Promise ((resolve, reject) => {
+    rl.question(question, (answer) => {
+      if (answer === "Yes") {
+        // Only resolve if answer === Yes
+        resolve(answer);
+      } else {
+        // If answer !== Yes then reject
+        reject(answer);
+      }
+    });
+  });
+}
